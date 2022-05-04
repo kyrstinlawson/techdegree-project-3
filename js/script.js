259d2c79a4e6912ab6ed2f0d66bfd9055c1afa6e
kyrstinlawson <kyrstin.lawson@gmail.com>
2022-05-04 11:41:44 -0700

    Added payment option changes

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ let shirtDesign = document.getElementById("design");
 let shirtOptions = shirtColor.children;
 let activities = document.getElementById("activities");
 let displayCost = document.getElementById("activities-cost");
-
+let paymentOption = document.getElementById("payment");
+let creditCard = document.getElementById("credit-card");
+let paypal = document.getElementById("paypal");
+let bitcoin = document.getElementById("bitcoin");
 
 // Focuses on name field on load
 nameField.focus();
@@ -53,3 +56,24 @@ activities.addEventListener("change", (e) => {
     displayCost.innerHTML = `Total: $${totalCost}`;
 });
 
+// Hides paypal and bitcoin options on load and selects the credit card option
+paypal.hidden = true;
+bitcoin.hidden = true;
+paymentOption.children[1].setAttribute("selected", true);
+
+// Changes payment info needed depending on payment option selected
+paymentOption.addEventListener("change", (e) => {
+    if (e.target.value === "paypal") {
+        paypal.hidden = false;
+        bitcoin.hidden = true;
+        creditCard.hidden = true;
+    } else if (e.target.value === "bitcoin") {
+        paypal.hidden = true;
+        bitcoin.hidden = false;
+        creditCard.hidden = true;
+    } else {
+        paypal.hidden = true;
+        bitcoin.hidden = true;
+        creditCard.hidden = false;
+    }
+});
